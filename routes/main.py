1f8f64082ded1da1ac37ff2e4c7902671ba48813
RenoMG <reno@renos.world>
2026-02-20 18:58:27 -0800

    cards arent unique by ID, use image_id and return card

--- routes/main.py
+++ routes/main.py
@@ -10,8 +10,18 @@ def index():
 @main.route("/lookup", methods=["POST"])
 def lookup():
     card_id = request.form["card_id"]
+    pick = request.form.get("pick_index")
+
     results = get_api_response(card_id)
+
+    if len(results) == 0:
+        return render_template("index.html", card=None, error="Card not found")
+    
+    if pick is not None:
+        card = results[int(pick)]
+        return render_template("index.html", card=card, prev_id=None, next_id=None)
 
     if len(results) >= 2:
         return render_template("index.html", card=None, choices=results)
+        
     return render_template("index.html", card=results[0], prev_id=None, next_id=None)
